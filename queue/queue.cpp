d9fb9552e2332d19da287a15b71b0774305c927d
Tyler Olson <tolson519@gmail.com>
2020-08-22 23:10:46 -0700

    and this one

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -1,10 +1,12 @@
 #include "queue.h"
 
 Queue::Queue() {}
-
 Queue::~Queue() {}
+
+std::ostream &operator<<(std::ostream &os, const Queue &q) {}
+std::ostream operator==(const Queue &q1, const Queue &q2){}
 
 void Queue::Enqueue(char c) {}
 void Queue::Dequeue() {}
 
-std::ostream &operator<<(std::ostream &os, const Queue &q) {}
+int Queue::Size() {}
