f68de652801b510bb41610f8727b6f0fe262fd2b
Tyler Olson <tolson519@gmail.com>
2020-08-27 16:15:46 -0700

    attempting something idk help

---
+++ ossimulator4.1/config.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void loadFile(std::string path, std::ifstream &config) {
+    config.open(path);
+}
+
+void closeFile(std::ifstream &stream) {
+    stream.close();
+}
+
+std::vector<std::string> parseConfig(std::ifstream &config) {
+    std::vector<std::string> values;
+
+    if (config.is_open()) {
+        std::string line;
+        while (getline(config, line)) {
+            values.push_back(line.substr(0, line.find(":")));
+        }
+    }
+
+    return values;
+}
+
+int getVariable(std::string label, std::ifstream &config, std::vector<std::string> &list) {
+    auto element = std::find(list.begin(), list.end(), label);
+    int variable;
+    if (element != list.end()) {
+        int index = std::distance(list.begin(), element);
+        variable = std::stoi(list[index + 1]);
+    }
+
+    return variable;
+}
+
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << i << " ";
+    }
+
+    std::cout << std::endl;
+    if (argc > 1) {
+        std::ifstream config;
+        loadFile(argv[1], config); //path, config stream
+        std::vector<std::string> parsedList = parseConfig(config);
+
+        int test = getVariable("value", config, parsedList);
+        std::cout << test << std::endl;
+        closeFile(config);
+    }
+    return 0;
+}
